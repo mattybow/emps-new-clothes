ee09f20c8c063c608300118b40f40fdd920ea1ea
bmagnantb <bmagnantb@gmail.com>
2015-05-14 12:19:28 -0500

    ip address retrieved instead of hard-coded

--- devServerUrl.js
+++ devServerUrl.js
@@ -1,4 +1,22 @@
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var localAddress;
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            localAddress = address.address;
+        }
+    }
+}
+
+
+// normal testing
 module.exports = 'localhost'
 
-// local IP for phone testing
-// module.exports = '192.168.1.101'
+// phone testing
+// module.exports = localAddress;
+
+// print ip to terminal for easy access
+// console.log(localAddress)
